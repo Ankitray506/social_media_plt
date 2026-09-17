37512b03122c140fb9a9b155069db86f54379a9e
Ankitray506 <210950370+Ankitray506@users.noreply.github.com>
2026-09-17 22:36:24 +0530

    Initial commit

---
+++ Frontend/src/pages/CreatePost.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const CreatePost = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        axios.post("http://localhost:3000/create-post",formData)
+         .then((res) => {
+
+        alert("Post created successfully")
+        e.target.reset()
+
+    })
+    .catch((err) => {
+
+        console.log(err)
+        alert("Error creating post")
+
+    })
+    }
+    return (
+        <section className='create-post-section'>
+            <h1>Create post</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="file" 
+                    name="image" 
+                    accept="image/*" 
+                />
+
+                <input 
+                    type="text" 
+                    name="caption" 
+                    placeholder='Enter caption'
+                    required 
+                />
+
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default CreatePost
